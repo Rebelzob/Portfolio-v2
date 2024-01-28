40e45ff5219ab8745039f130c4c088e0c4d2c940
Rebelzob <karimdavid07@gmail.com>
2024-01-28 17:36:15 +0000

    Add backdropBlur utility classes to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backdropBlur: {
+				'none': '0',
+				'sm': '4px',
+        'DEFAULT': '8px',
+        'md': '12px',
+        'lg': '16px',
+        'xl': '24px',
+        '2xl': '40px',
+        '3xl': '64px',
+			}
+		},
+	},
+	variants: {
+		extend:{
+			backdropBlur: ['responsive']
+		}
 	},
 	plugins: [],
 }
